6fcaccb8880d29d7149c6e3ff70318a7dde45e6b
James Batten <jamesbatten42@gmail.com>
2016-06-07 23:41:02 +0200

    cleaned up some code

--- plotting/plotter.py
+++ plotting/plotter.py
@@ -6,10 +6,6 @@ def plot_digit(digit):
 
     fig=plt.figure()
     ax=fig.add_subplot(111)
-    #all_data = [[1,10],[2,10],[3,10],[4,10],[5,10],[3,1],[3,2],[3,3],[3,4],[3,5]]
-    #all_data = digit.curves[0]
-    #plt.plot(*zip(*itertools.chain.from_iterable(itertools.combinations(all_data, 2))),color = 'brown', marker = 'o')
-
 
     for curve in digit.curves:
         x_points = []
