0b74ec23a475525ef1dc35d3bdf4020f5a579ef2
James Batten <orkat42@gmail.com>
2016-06-15 00:37:09 +0200

    implemented sample heatmap

--- plotting/plotter.py
+++ plotting/plotter.py
@@ -118,7 +118,7 @@ def plot_digit_observations(digit, centroids, n_observation_classes, display_pro
     plt.show()
 
 
-def plot_digits_samples(samples, display_progress = False):
+def plot_digit_samples(samples, display_progress = False):
 
     f = FloatProgress(min=0, max=100)
     if display_progress:
@@ -131,22 +131,58 @@ def plot_digits_samples(samples, display_progress = False):
         for j in range(0, 5):
 
             n = 5*i + j
+            n -= 1
+            if n < 0:
+                n = 9
+
 
             x_points = []
             y_points = []
-            for digit in digits:
-                if digit.label == n:
-                    for curve in digit.curves:
-                        for point in curve:
-                            x_points.append(point[0])
-                            y_points.append(point[1])
 
-            heatmap, xedges, yedges = np.histogram2d(x_points, y_points, bins=50);
+            for curve in samples[n][0].curves:
+                for point in curve:
+                    x_points.append(point[0])
+                    y_points.append(point[1])
+
+            axarr[i, j].plot(x_points, y_points, linewidth = 2.0)
+            #axarr[i, j].axis([settings.IMAGE_PLOT_X_MIN, settings.IMAGE_PLOT_X_MAX, settings.IMAGE_PLOT_Y_MIN, settings.IMAGE_PLOT_Y_MAX]);
+            f.value += 10
+
+    f.close()
+    plt.show();
+
+
+def plot_digit_samples_heatmap(samples, display_progress = False):
+
+    f = FloatProgress(min=0, max=100)
+    if display_progress:
+        display(f)
+
+    plt.clf();
+    _, axarr = plt.subplots(2, 5);
+
+    for i in range(0, 2):
+        for j in range(0, 5):
+
+            n = 5*i + j
+            n -= 1
+            if n < 0:
+                n = 9
+
+            x_points = []
+            y_points = []
+            for dig in samples[n]:
+                for curve in dig.curves:
+                    for point in curve:
+                        x_points.append(point[0])
+                        y_points.append(point[1])
+
+            heatmap, xedges, yedges = np.histogram2d(x_points, y_points, bins=30);
 
             extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]];
 
             axarr[i, j].imshow(np.rot90(heatmap), extent=extent);
-            #axarr[i, j].axis([settings.IMAGE_PLOT_X_MIN, settings.IMAGE_PLOT_X_MAX, settings.IMAGE_PLOT_Y_MIN, settings.IMAGE_PLOT_Y_MAX]);
+
             f.value += 10
 
     f.close()
