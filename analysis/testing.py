bea7dbe0a9dfc4352da121212ffd8fed15339588
James Batten <orkat42@gmail.com>
2016-06-19 12:09:09 +0200

    implemented gaussian hmms

--- analysis/testing.py
+++ analysis/testing.py
@@ -11,108 +11,28 @@ def evaluate_classification_accuracy(test_digits, predicted_labels):
     n_correct = 0
     n_false = 0
 
+    n_correct_labels = [0,0,0,0,0,0,0,0,0,0]
+    n_false_labels = [0,0,0,0,0,0,0,0,0,0]
+    predictions = []
+    for i in range(0, 10):
+        predictions.append([0,0,0,0,0,0,0,0,0,0])
+
+
     i = 0
     for dig in test_digits:
 
+        predictions[dig.label][predicted_labels[i]] += 1
         if dig.label == predicted_labels[i]:
             n_correct += 1
+            n_correct_labels[dig.label] += 1
         else:
             n_false += 1
+            n_false_labels[dig.label] += 1
 
         i += 1
 
+    for i in range(0, 10):
+        print("label " + str(i) + " : " + str(float(n_correct_labels[i]) / (float(n_correct_labels[i]) + float(n_false_labels[i]))), end = "")
+        print("  ,  predictions : " + str(predictions[i]))
+
     return float(n_correct) / (float(n_correct) + float(n_false))
-
-def predict_labels(test_digits, hidden_markov_models, centroids, n_observation_classes, n_hidden_states, n_iter, tol, display_progress):
-
-    labels = []
-
-    directory = settings.PREDICTED_LABELS_DIRECTORY + "centroids_" + str(n_observation_classes - 3)
-    directory += "/hidden_states_" + str(n_hidden_states) + "/n_iter_" + str(n_iter) + "/tol_" + str(tol)
-    filename = "predicted_digit_labels.dat"
-    path = directory + "/" + filename
-    if os.path.isfile(path):
-        labels = pickle.load(open(path, 'rb'))
-    else:
-
-        f = FloatProgress(min=0, max=100)
-        if display_progress:
-            display(f)
-
-        i = 0
-        for dig in test_digits:
-            label = predict_label(dig, hidden_markov_models, centroids, n_observation_classes)
-            labels.append(label)
-            f.value = (float(i) * 100.0) / float(len(test_digits))
-            i += 1
-
-        f.close()
-
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        with open(path,'wb') as f:
-            pickle.dump(labels,f)
-
-    return labels
-
-def predict_label(test_digit, hidden_markov_models, centroids, n_observation_classes):
-
-    max_score_value = 0
-    max_score_label = 0
-    max_score_set = False
-
-    for hidden_markov_model in hidden_markov_models:
-
-        score = get_hmm_probability(test_digit, hidden_markov_model, centroids, n_observation_classes)
-
-        if not max_score_set or score > max_score_value:
-
-            max_score_set = True
-            max_score_value = score
-            max_score_label = hidden_markov_model.digit_label
-
-    return max_score_label
-
-
-def get_hmm_probability(test_digit, hidden_markov_model, centroids,  n_observation_classes):
-
-    pen_down_label = n_observation_classes - settings.PEN_DOWN_LABEL_DELTA
-    pen_up_label = n_observation_classes - settings.PEN_UP_LABEL_DELTA
-    stop_label = n_observation_classes - settings.STOP_LABEL_DELTA
-
-    special_observations = [pen_down_label, pen_up_label, stop_label]
-    train_digit_observations = [pen_down_label]
-
-    for curve in test_digit.curves:
-        for point in curve:
-
-            min_dist_value = -1.0
-            min_dist_obs = -1
-
-            for obs in hidden_markov_model.digit_observations:
-
-                if obs not in special_observations:
-
-                    dx = centroids[obs][0] - point[0]
-                    dy = centroids[obs][1] - point[1]
-
-                    d2 = dx*dx + dy*dy
-
-                    if min_dist_obs == -1 or d2 < min_dist_value:
-                        min_dist_obs = obs
-                        min_dist_value = d2
-
-            train_digit_observations.append(min_dist_obs)
-        train_digit_observations.append(pen_up_label)
-    train_digit_observations.append(stop_label)
-
-    encoded_train_digit_observations = []
-    for obs in train_digit_observations:
-        encoded_train_digit_observations.append(hidden_markov_model.label_encoder.transform(obs))
-
-    X = np.array(encoded_train_digit_observations)
-    lengths = [len(encoded_train_digit_observations)]
-
-    score = hidden_markov_model.hidden_markov_model.score(np.atleast_2d(X).T, lengths)
-
-    return score
--- analysis/training.py
+++ analysis/training.py
@@ -6,7 +6,8 @@ import pickle
 import os.path
 import hmmlearn.hmm as hmm
 import numpy as np
-
+from ipywidgets import FloatProgress
+from IPython.display import display
 
 def get_digit_kmeans_centroids(digits, n_clusters):
 
@@ -118,7 +119,83 @@ def train_hmm(digits, n_observation_classes, n_hidden_states, n_iter, tol):
             with open(path,'wb') as f:
                 pickle.dump(hidden_markov_model,f)
 
+
+    check_hidden_markov_models(hidden_markov_models)
     return hidden_markov_models
+
+
+def train_gaussian_hmm(digits, n_hidden_states, n_iter, tol):
+
+    hidden_markov_models = []
+
+    for i in range(0, 10):
+
+        digit_label = i + 1
+        if digit_label == 10:
+            digit_label = 0
+
+        directory = settings.GAUSSIAN_HIDDEN_MARKOV_MODE_DIRECTORY
+        directory += "/hidden_states_" + str(n_hidden_states) + "/n_iter_" + str(n_iter) + "/tol_" + str(tol)
+        filename = "digit_label_" + str(digit_label) + ".dat"
+        path = directory + "/" + filename
+        if os.path.isfile(path):
+            hidden_markov_model = pickle.load(open(path, 'rb'))
+            hidden_markov_models.append(hidden_markov_model)
+        else:
+
+            current_digits = []
+            samples = [] # concatenated observation sequences
+            lengths = [] # lengths of observation sequences in samples
+            for dig in digits:
+                if dig.label == digit_label:
+                    current_digits.append(dig)
+                    points_len = 0
+                    for curve in dig.curves:
+                        for point in curve:
+                            samples.append(point)
+                            points_len += 1
+                    lengths.append(points_len)
+
+            #print(samples)
+
+            X = np.array(samples)
+
+            h = hmm.GaussianHMM(n_components=n_hidden_states, verbose=settings.HIDDEN_MARKOV_MODEL_VERBOSE, n_iter=n_iter, tol=tol)
+            h.fit(np.atleast_2d(X), lengths)
+
+            hidden_markov_model = GaussianHiddenMarkovModel(h, digit_label)
+            hidden_markov_models.append(hidden_markov_model)
+
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            with open(path,'wb') as f:
+                pickle.dump(hidden_markov_model,f)
+
+
+    check_hidden_markov_models(hidden_markov_models)
+    return hidden_markov_models
+
+
+
+def check_hidden_markov_models(hidden_markov_models):
+
+    for hidden_markov_model in hidden_markov_models:
+
+        transmat = hidden_markov_model.hidden_markov_model.transmat_
+        height = transmat.shape[0]
+        width = transmat.shape[1]
+
+        for i in range(0, height):
+            rowsum = 0.0
+            for j in range(0, width):
+                rowsum += transmat[i][j]
+            if rowsum < 0.5:
+                transmat[i][i] = 1.0 - rowsum
+
+        hidden_markov_model.hidden_markov_model.transmat_ = transmat
+
+
+
 
 class HiddenMarkovModel():
 
@@ -128,6 +205,32 @@ class HiddenMarkovModel():
         self.label_encoder = label_encoder
         self.digit_label = digit_label
         self.digit_observations = digit_observations
+
+def initialise_random_transition_matrix(n_hidden_states):
+
+    transmat = np.random.rand(n_hidden_states, n_hidden_states)
+    row_sums = transmat.sum(axis=1)
+    return transmat / row_sums[:, np.newaxis]
+
+def initialise_random_emission_matrix(n_hidden_states, n_observation_classes):
+
+    emitmat = np.random.rand(n_hidden_states, n_observation_classes)
+    row_sums = emitmat.sum(axis=1)
+    return emitmat / row_sums[:, np.newaxis]
+
+def initialise_random_start_probability_matrix(n_hidden_states):
+
+    startprob = np.random.rand(1, n_hidden_states)
+    row_sums = startprob.sum(axis=1)
+    return startprob / row_sums[:, np.newaxis]
+
+
+class GaussianHiddenMarkovModel():
+
+    def __init__(self, multinomialHMM, digit_label):
+
+        self.hidden_markov_model = multinomialHMM
+        self.digit_label = digit_label
 
 def initialise_random_transition_matrix(n_hidden_states):
 
